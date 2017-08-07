ccb601f1920b146e0bd1d8aa808e99d1626d69a5
kaka <734761114@qq.com>
2017-08-07 18:30:55 +0800

    123

--- src/main/java/com/dev/demo/controller/Regist.java
+++ src/main/java/com/dev/demo/controller/Regist.java
@@ -3,7 +3,7 @@ package com.dev.demo.controller;
 public class Regist {
 
 	public Regist() {
-		System.out.println("this is master branch");
+		System.out.println("this is master branch 123");
 	}
 
 }
